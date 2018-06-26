a3098d203247660e512d1ec53907261bde08ea68
Owner <brunocandia@gmail.com>
2018-06-26 00:32:58 -0300

    Add project files.

---
+++ Features/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers2 = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Juan" },
+                new Employee { Id = 2, Name = "Jose" }
+            };
+
+            // Otra forma de hacer foreach
+            IEnumerator<Employee> enumeratorDevelopers2 = developers2.GetEnumerator();
+            while (enumeratorDevelopers2.MoveNext())
+            {
+                Console.WriteLine(enumeratorDevelopers2.Current.Name);
+            }
+
+            foreach (var person in developers2)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            IEnumerable<Employee> sales2 = new List<Employee>
+            {
+                new Employee { Id = 3, Name = "Pedro" },
+                new Employee { Id = 4, Name = "Pablo" }
+            };
+
+            // Otra forma de hacer foreach
+            IEnumerator<Employee> enumeratorSales2 = sales2.GetEnumerator();
+            while (enumeratorSales2.MoveNext())
+            {
+                Console.WriteLine(enumeratorSales2.Current.Name);
+            }
+
+            // Una forma de llamar al extension method
+            Console.WriteLine(MyLinq.Count(sales2));
+
+            // Otra forma de llamar al extension method
+            Console.WriteLine(sales2.Count());
+
+            foreach (var person in sales2)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            Console.WriteLine("**********");
+            /*******************************************/
+
+            Employee[] developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Juan" },
+                new Employee { Id = 2, Name = "Jose" }
+            };
+
+            foreach (var person in developers)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            List<Employee> sales = new List<Employee>
+            {
+                new Employee { Id = 3, Name = "Pedro" },
+                new Employee { Id = 4, Name = "Pablo" }
+            };                       
+
+            foreach (var person in sales)
+            {
+                Console.WriteLine(person.Name);
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Introduction/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\windows";
+
+            ShowLargeFilesWithoutLinq(path);
+
+            Console.WriteLine("**********");
+
+            ShowLargeFilesWithLinq(path);
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        private static void ShowLargeFilesWithLinq(string path)
+        {
+            var querySyntax = from file in new DirectoryInfo(path).GetFiles()
+                              orderby file.Length descending
+                              select file;
+
+            foreach (var file in querySyntax.Take(5))
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+
+            Console.WriteLine("**********");
+
+            var methodSyntax = new DirectoryInfo(path).GetFiles()
+                        .OrderByDescending(f => f.Length)
+                        .Take(5);
+
+            foreach (var file in methodSyntax)
+            {
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+        }
+
+        private static void ShowLargeFilesWithoutLinq(string path)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles();
+            Array.Sort(files, new FileInfoComparer());
+
+            for (int i = 0; i < 5; i++)
+            {
+                var file = files[i];
+                Console.WriteLine($"{file.Name,-20} : {file.Length,10:N0}");
+            }
+
+            //foreach (var file in files)
+            //{
+            //    Console.WriteLine($"{file.Name} : {file.Length}");
+            //}
+        }
+    }
+
+    public class FileInfoComparer : IComparer<FileInfo>
+    {
+        public int Compare(FileInfo x, FileInfo y)
+        {
+            return y.Length.CompareTo(x.Length);
+        }
+    }
+}
