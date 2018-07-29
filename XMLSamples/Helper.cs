41bd2394534edfd82a05cb00722fae9e9f1cf9e7
Owner <brunocandia@gmail.com>
2018-07-29 13:45:52 -0300

    Helper class added

---
+++ XMLSamples/Helper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XMLSamples
+{
+    public class Helper
+    {
+        public Helper()
+        {
+
+        }
+    }
+}
